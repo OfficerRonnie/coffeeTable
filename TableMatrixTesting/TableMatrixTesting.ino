135c3ddc41c6d79c4eef9798697062ade31ca6f5
OfficerRonnie <ron@ronjenks.com>
2014-10-28 13:16:34 -0700

    Matrix Testing
    I'm trying to test to see if i can get a working matrix to program the
    LEDS easier

---
+++ TableMatrixTesting/TableMatrixTesting.ino
@@ -0,0 +1,22 @@
+#include "FastLED.h"
+
+#define NUM_LEDS 512
+#define DATA_PIN 75
+
+CRGB leds[NUM_LEDS];
+
+int matrix[16][32];
+
+void setup()
+{
+
+  /* add setup code here */
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
